f08d76bbe0db226dbff835069c3ba741042942f9
usver <em.92@mail.ru>
2020-01-05 18:04:20 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+var driver = new Builder().forBrowser('chrome').build();
+var where = 'https://www.shazam.com/charts/top-200/ukraine';
+var what = '.play:nth-child(1)';
+
+try {
+    driver.get(where)
+        .then(_ => driver.wait(until.elementLocated(By.css(what))))
+        .then(_ => driver.findElement(By.css(what)).click())
+        // .then(_ => driver.quit());
+} catch (err){
+    console.log(err)
+}
