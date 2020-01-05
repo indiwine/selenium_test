ff7aaaefcc6f9b50a827be284488954e8c08f30f
usver <em.92@mail.ru>
2020-01-05 20:05:08 +0200

    init commit1

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const { Builder, By, Key, until } = require("selenium-webdriver");
+var driver = new Builder().forBrowser("chrome").build();
+var where = "https://www.shazam.com/charts/top-200/ukraine";
+var what = ".play:nth-child(1)";
+var what2 = ".tracks";
+var tracks = [];
 
-var driver = new Builder().forBrowser('chrome').build();
-var where = 'https://www.shazam.com/charts/top-200/ukraine';
-var what = '.play:nth-child(1)';
-
-try {
-    driver.get(where)
-        .then(_ => driver.wait(until.elementLocated(By.css(what))))
-        .then(_ => driver.findElement(By.css(what)).click())
-        .then(_ => driver.findElement(By.css(what)).click())
-        .then(_ => setTimeout(() => {  driver.quit() }, 20000))
-} catch (err){
-    console.log(err)
-}
+driver.get(where);
+driver
+  .wait(until.elementLocated(By.css(what2)))
+  .then(_ => driver.findElement(By.css(what2)).getAttribute("innerHTML"))
+  .then(_ => {
+    tracks.push(_);
+    console.log(_);
+  })
+  .then(
+    setTimeout(() => {
+      console.log(tracks);
+      driver.close();
+      driver.quit();
+    }, 5000)
+  );
+// driver.findElement(By.css(what2)).getAttribute("innerHTML").then(_ => {tracks = _; console.log(tarcks)});
