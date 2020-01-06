7cfd21ebe92de8a3b6301824359aa0035f652528
usver <em.92@mail.ru>
2020-01-06 13:09:24 +0200

    part

--- index.js
+++ index.js
@@ -1,25 +1,58 @@
 const {Builder, By, until} = require('selenium-webdriver');
+const driver = new Builder().forBrowser('chrome').build();
 const fs = require('fs');
 
 var where = "https://www.shazam.com/charts/top-200/ukraine";
 var who = "div.title";
 var what = "div.artist";
 var number = "span.number";
-var item = "li[itemprop='track']";
+var tr = "ul.tracks li[itemprop='track']";
 var what2 = ".tracks";
+var model = {};
+var item = {};
+var items = {};
 var tracks = [];
+var i = 0;
+var temp='';
+var good50 = [];
+
+async function getText(a,b){
+    let elements = await driver.findElements(By.css(a));
+    if (Array.isArray(b)){
+        for(let e of elements) {
+            
+            b.push(await e.getText());
+        }
+        // console.log(temp)
+        b.forEach(element => {i+=1;item['it'+i]={"song":element};items['it'+i]=item['it'+i]});
+        console.log(items)
+    }else{
+        console.error("second argument is not an array")
+    }
+}
+
+function getClear50(){
+    let elements = await driver.findElements(By.css(tr));
+        for(let e of elements) {
+            e.getAttribute('class').then(text => temp = text)
+            if (temp == 'last-visible') break;
+            good50.push(e);
+            // tracks.push(await e.getText());
+        }
+}
 
 (async function example() {
-    let driver = await new Builder().forBrowser('chrome').build();
     try {
         await driver.get(where);
         await driver.wait(until.elementLocated(By.css(what)));
-        let elements = await driver.findElements(By.css(item));
-        for(let e of elements) {
-            console.log(await e.getText());
-            tracks.push(await e.getText());
-        }
-        fs.writeFile("new.txt",tracks,(err)=> {if (err) throw err; console.log('file is ok')})
+        getClear50();
+
+        // await getText(who,tracks);
+        // tracks.forEach(element => {i+=1;item['it'+i]={"song":element};items['it'+i]=item['it'+i]});
+        // console.log(items)
+        // console.log(JSON.stringify(items));
+        // fs.writeFile("new.txt",tracks,(err)=> {if (err) throw err; console.log('file is ok')})
+        // fs.writeFile("artists.txt",JSON.parse(items),(err)=> {if (err) throw err; console.log('file is ok')})
     }
     finally {
         await driver.quit();
