a21692bc3c4dae6284df85db7cc9e72c06cf4956
usver <em.92@mail.ru>
2020-01-08 02:00:40 +0200

    pred alpha v1.0

--- index.js
+++ index.js
@@ -7,41 +7,52 @@ const https = require("https");
 const ffmpeg = require("fluent-ffmpeg");
 const merge = ffmpeg();
 const dir = "./files";
-var ghj=0;
+var ghj = 0;
 var fileList = [];
 var listFileName = "list.txt";
 var fileNames = "";
 
-// reading and writing track list to file
-//88
-function concatAudio() {
-  fs.readdir(dir, (err, files) => {
-    files.forEach(file => {
-      fileList.push(file);
-    });
-    console.log(fileList);
-    fileList.forEach(fileName => {
-      fileNames = fileNames + "file '" + dir + "/" + fileName + "'\n";
-    });
-    console.log(fileNames);
-    fs.writeFileSync(listFileName, fileNames);
-  });
+var where = "https://www.shazam.com/charts/top-200/ukraine";
+var what = "div.artist";
+var tr = "ul.tracks li[itemprop='track']";
+var tracks = [];
+var urlSh = [];
+var urlShMusExt = [];
+var urlShortMusic = [];
+var urlImg = [];
+var temp = "";
+var temp2 = "";
 
-  merge
-    .input(listFileName)
-    .inputOptions(["-f concat", "-safe 0", "-report"])
-    .outputOptions("-c copy")
-    .save("./readyToUpload/mergedAudio.m4a")
-    .on("error", function(err) {
-      console.log("Error " + err.message);
-    })
-    .on("end", function() {
-      console.log("Finished!");
-    });
-}
-//88
-// downloading SHORT tracks
-//11
+// // reading and writing track list to file
+// //88
+// function concatAudio() {
+//   fs.readdir(dir, (err, files) => {
+//     files.forEach(file => {
+//       fileList.push(file);
+//     });
+//     console.log(fileList);
+//     fileList.forEach(fileName => {
+//       fileNames = fileNames + "file '" + dir + "/" + fileName + "'\n";
+//     });
+//     console.log(fileNames);
+//     fs.writeFileSync(listFileName, fileNames);
+//   });
+
+//   merge
+//     .input(listFileName)
+//     .inputOptions(["-f concat", "-safe 0", "-report"])
+//     .outputOptions("-c copy")
+//     .save("./readyToUpload/mergedAudio.m4a")
+//     .on("error", function(err) {
+//       console.log("Error " + err.message);
+//     })
+//     .on("end", function() {
+//       console.log("Finished!");
+//     });
+// }
+// //88
+// // downloading SHORT tracks
+
 var removeFromFile = new Promise(function(resolve, reject) {
   fs.readdir(dir, (err, files) => {
     try {
@@ -50,8 +61,6 @@ var removeFromFile = new Promise(function(resolve, reject) {
           fs.unlinkSync(dir + "/" + file);
         });
         resolve("all files deleted");
-      } else {
-        resolve("there is no files");
       }
     } catch (err) {
       console.log(err);
@@ -69,10 +78,10 @@ function download(url, dest, cb) {
         file.close(cb); // close() is async, call cb after close completes.
       });
     })
-    .on('close', function(){
-        ghj=ghj+1;
-        // console.log('file downloaded');
-        console.log(ghj);
+    .on("close", function() {
+      ghj = ghj + 1;
+      // console.log('file downloaded');
+      console.log(ghj);
     })
     .on("error", function(err) {
       // Handle errors
@@ -80,48 +89,6 @@ function download(url, dest, cb) {
       if (cb) cb(err.message);
     });
 }
-
-//11
-
-var where = "https://www.shazam.com/charts/top-200/ukraine";
-var what = "div.artist";
-var tr = "ul.tracks li[itemprop='track']";
-var tracks = [];
-var urlSh = [];
-var urlShMusExt = [];
-var urlShortMusic = [];
-var urlImg = [];
-var temp = "";
-var temp2 = "";
-
-// async function getText(a, b) {
-//   let elements = await driver.findElements(By.css(a));
-//   var itemKey = "";
-//   if (a == who) itemKey = "song";
-//   //   if (a == who) itemKey= 'range'
-//   //   if (a == who) itemKey= 'artist'
-//   //   if (a == who) itemKey= 'img'
-//   //   if (a == who) itemKey= 'shortMusicUrl'
-//   //   if (a == who) itemKey= 'MusicUrl'
-//   //   if (a == who) itemKey= 'ShazamUrl'
-//   //   if (a == who) itemKey= 'AppleMusicUrl'
-//   console.log(a);
-//   if (Array.isArray(b)) {
-//     for (let e of elements) {
-//       b.push(await e.getText());
-//     }
-//     // console.log(b)
-//     b.forEach(element => {
-//       i += 1;
-//       item["it" + i] = { [itemKey]: element };
-//       // item.song = element;
-//       items["it" + i] = item["it" + i];
-//     });
-//     console.log(items);
-//   } else {
-//     console.error("second argument is not an array");
-//   }
-// }
 
 async function getClear50() {
   let elements = await driver.findElements(By.css(tr));
@@ -187,26 +154,6 @@ async function sortCreate() {
   // console.log(ar4);
 }
 
-var ast1 = "";
-
-var qsd = new Promise(function(resolve, reject) {
-    var i = 0;
-    var ast = "";
-    for (e in ar4) {
-      i = i + 1;
-      if (i < 10) {
-        ast = "0" + i;
-      }
-      if (i >= 10) {
-        ast = i;
-      }
-      download(ar4[e].urlShortMusic, dir + "/" + ast + ar4[e].urlShMusExt);
-    }
-    if(ghj==50){
-        resolve('ok')
-    }  
-})
-
 function cyclicDownload() {
   var i = 0;
   var ast = "";
@@ -220,57 +167,29 @@ function cyclicDownload() {
     }
     download(ar4[e].urlShortMusic, dir + "/" + ast + ar4[e].urlShMusExt);
   }
-//   return ghj
 }
 
-// var tyu = new Promise(function(resolve, reject) {
-  (async function example() {
-    try {
-      await driver.get(where);
-      await driver.wait(until.elementLocated(By.css(what)));
-      await getClear50();
-      await sortCreate();
-      await removeFromFile
-        .then(res => {
-          qsd;
-          console.log(res);
-        }).then(_ =>{if (_==ok){concatAudio()}})
-        .catch(err => console.log(err));
-    //   console.log(ghj);
+(async function example() {
+  try {
+    await driver.get(where);
+    await driver.wait(until.elementLocated(By.css(what)));
+    await getClear50();
+    await sortCreate();
+    await removeFromFile
+      .then(res => {
+        cyclicDownload();
+        console.log(res);
+      })
+      .catch(err => console.log(err));
 
     //   await concatAudio();
-      // await fs.writeFile("new.txt",tracks,(err)=> {if (err) throw err; console.log('file is ok')})
-      // tracks.forEach(element => {i+=1;item['it'+i]={"song":element};items['it'+i]=item['it'+i]});
-      // console.log(JSON.stringify(items));
-      // fs.writeFile("artists.txt",JSON.parse(items),(err)=> {if (err) throw err; console.log('file is ok')})
-    } catch (err) {
-      console.log(err);
-    } finally {
-      // concatAudio();
-    //   resolve("ok");
-      await driver.quit();
-    }
-  })();
-// });
-
-
-
-// function check(callback) {
-//     // do a bunch of stuff
-//     if (ghj==50) {
-//         // call the callback to notify other code
-//         callback();
-//     }
-// }
-
-// tyu
-//   .then(_ => {
-//     concatAudio();
-//     console.log(_);
-//   })
-//   .catch(err => console.log(err));
-
-/* to do  
-0. rename variables
-1. album "image album-art flex-reset  img-audio-play" in style='background-image: url("https://images.shazam.com/coverart/t466040067-b1475546088_s400.jpg");'
-2. short sound <meta itemprop="audio" content="https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview123/v4/22/e5/8e/22e58eeb-5916-c584-257c-735e1b4b0175/mzaf_17966999117302129087.plus.aac.p.m4a">*/
+    // await fs.writeFile("new.txt",tracks,(err)=> {if (err) throw err; console.log('file is ok')})
+    // tracks.forEach(element => {i+=1;item['it'+i]={"song":element};items['it'+i]=item['it'+i]});
+    // console.log(JSON.stringify(items));
+    // fs.writeFile("artists.txt",JSON.parse(items),(err)=> {if (err) throw err; console.log('file is ok')})
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await driver.quit();
+  }
+})();
--- index2.js
+++ index2.js
@@ -1,9 +1,5 @@
-const { Builder, By, until } = require("selenium-webdriver");
-const driver = new Builder().forBrowser("chrome").build();
 
 const fs = require("fs");
-const http = require("http");
-const https = require("https");
 const ffmpeg = require("fluent-ffmpeg");
 const merge = ffmpeg();
 const dir = "./files";
@@ -12,8 +8,6 @@ var fileList = [];
 var listFileName = "list.txt";
 var fileNames = "";
 
-// reading and writing track list to file
-//88
 function concatAudio() {
   fs.readdir(dir, (err, files) => {
     files.forEach(file => {
@@ -41,29 +35,3 @@ function concatAudio() {
 }
 
 concatAudio();
-
-// (async function example() {
-//   try {
-//     // await driver.get(where);
-//     // await driver.wait(until.elementLocated(By.css(what)));
-//     // await getClear50();
-//     // await sortCreate();
-//     // await removeFromFile.then(res => cyclicDownload()).catch(err => console.log(err));
-//     // await concatAudio();
-//     // await fs.writeFile("new.txt",tracks,(err)=> {if (err) throw err; console.log('file is ok')})
-//     // await getText(who,tracks);
-//     // await getText(what,tracks);
-//     // await getText(who,tracks);
-//     // tracks.forEach(element => {i+=1;item['it'+i]={"song":element};items['it'+i]=item['it'+i]});
-//     // console.log(items)
-//     // console.log(JSON.stringify(items));
-//     // fs.writeFile("artists.txt",JSON.parse(items),(err)=> {if (err) throw err; console.log('file is ok')})
-//   } finally {
-//     await driver.quit();
-//   }
-// })();
-
-/* to do  
-0. rename variables
-1. album "image album-art flex-reset  img-audio-play" in style='background-image: url("https://images.shazam.com/coverart/t466040067-b1475546088_s400.jpg");'
-2. short sound <meta itemprop="audio" content="https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview123/v4/22/e5/8e/22e58eeb-5916-c584-257c-735e1b4b0175/mzaf_17966999117302129087.plus.aac.p.m4a">*/
